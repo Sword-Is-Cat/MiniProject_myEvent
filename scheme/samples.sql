90b34590bbb0a27dbea00710017656171c43ca7f
skyvault05 <kimzombie@hotmail.com>
2020-04-23 21:54:14 +0900

    testing backup branch

--- scheme/samples.sql
+++ scheme/samples.sql
@@ -1,7 +1,7 @@
 SELECT * FROM users;
 
-INSERT INTO users(userno, userid, userpwd, username, useraddr, userphone, useremail)
-VALUES (1, 'test', '1234', '테스트이름', '주소','000-0000-0000', 'ddd@gmail.com');
+INSERT INTO users(userid, userpwd, username, useraddr, userphone, useremail)
+VALUES ('test', '1234', '테스트이름', '주소','000-0000-0000', 'ddd@gmail.com');
 
 ALTER TABLE users MODIFY userno DEFAULT USERSEQ.NEXTVAL;
 
