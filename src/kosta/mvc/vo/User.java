052ab7096665e488ae2d4ab2a5ce926d169915a5
skyvault05 <kimzombie@hotmail.com>
2020-04-24 12:31:36 +0900

    HJ 모든 테이블에 대응하는 V생성

---
+++ src/kosta/mvc/vo/User.java
@@ -0,0 +1,96 @@
+package kosta.mvc.vo;
+
+public class User {
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String userAddr;
+	private String userPhone;
+	private String userEmail;
+	private long userJoinDate;
+	private int userStatus;
+	
+	public User() {};
+	/**
+	 * 가입용
+	 * @param userId
+	 * @param userPwd
+	 * @param userName
+	 * @param userAddr
+	 * @param userPhone
+	 * @param userEmail
+	 */
+	public User(String userId, String userPwd, String userName, String userAddr, String userPhone, String userEmail) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.userAddr = userAddr;
+		this.userPhone = userPhone;
+		this.userEmail = userEmail;
+	}
+	public User(int userNo, String userId, String userPwd, String userName, String userAddr, String userPhone,
+			String userEmail, long userJoinDate, int userStatus) {
+		this(userId, userPwd, userName, userAddr, userPhone, userEmail);
+		this.userNo = userNo;
+		this.userJoinDate = userJoinDate;
+		this.userStatus = userStatus;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserAddr() {
+		return userAddr;
+	}
+	public void setUserAddr(String userAddr) {
+		this.userAddr = userAddr;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public long getUserJoinDate() {
+		return userJoinDate;
+	}
+	public void setUserJoinDate(long userJoinDate) {
+		this.userJoinDate = userJoinDate;
+	}
+	public int getUserStatus() {
+		return userStatus;
+	}
+	public void setUserStatus(int userStatus) {
+		this.userStatus = userStatus;
+	}
+	
+	
+}
